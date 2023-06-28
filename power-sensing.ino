639922d9a618d7c77d97abfa567bcd0b38c85045
Kristin Ebuengan <ebuengankris@gmail.com>
2023-06-28 14:59:07 -0700

    make print statements more readable

--- power-sensing.ino
+++ power-sensing.ino
@@ -13,28 +13,29 @@ void loop() {
 
   measurement = analogRead(voltagePin);
   Serial.print("received (voltage) = ");
-  Serial.print(measurement);
+  Serial.println(measurement);
 
   analog = (measurement * 5.00)/1023.00;
-  Serial.print("    voltage = ");
-  Serial.print(analog);
+  Serial.print("voltage = ");
+  Serial.println(analog);
   voltage = analog * 0.5;
-  Serial.print("    voltage (halved) = ");
+  Serial.print("voltage (halved) = ");
   Serial.println(voltage);
 
   float vout;
   float current;
   measurement = analogRead(currentPin);
   Serial.print("received (current) = ");
-  Serial.print(measurement);
+  Serial.println(measurement);
   
   vout = (measurement * 5.00)/1023.00;
-  Serial.print("    vout = ");
-  Serial.print(vout);
+  Serial.print("vout = ");
+  Serial.println(vout);
   
   current = float(vout) / float((100*2.2));
-  Serial.print("    current = ");
+  Serial.print("current = ");
   Serial.println(current, 5);
+  Serial.println();
 
   delay(1000);
 }
