dbf6344065655c412c86b6af20abb8821acbaedd
Kristin Ebuengan <ebuengankris@gmail.com>
2023-07-06 11:21:13 -0700

    styled the repo

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# power-sensing
+# Power-Sensing
+
+## How to calulate the voltage, current, and power using the Soil Power Sensor Board
+
+Refer to the Soil_Power_Sensing_Board_Manual.pdf on the repo to get the detials of how the Soil Power Sensor Board works.
+
+**Voltage:**
+A0 pin on the Arduino Uno is what you want to connect to the voltage you want to measure. The voltage you will receive is will be the voltage x2, so to see the true measurment of voltage you would want to half the amount you recieve.
+
+NOTICE: the voltage will cap out at 2.4 V as this board was specifically made for Microbial Fuel Cells (MFCs) in mind. 
+
+**Current:**
+A1 pin on the Arduino Uno is what you want to connect to the voltage you want to measure
+
+NOTICE: there aren't any precautions to prevent a circuit shortage
--- power-sensing.ino
+++ power-sensing.ino
@@ -1,7 +1,12 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText 2023 Kristin Ebuengan
+// SPDX-FileCopyrightText 2023 Melody Gill
+// SPDX-FileCopyrightText 2023 Gabriel Marcano
+
 /*
 * Read voltage and current using Soil Power Board with an Arduino Uno.
 * Take samples once a large enough voltage is detected, and print them to
-* serial after all the samples are taken.
+* serial in CSV format after all the samples are taken.
 */
 
 #include <Vector.h>
@@ -24,85 +29,41 @@ Vector<int> currentArray(arr2, 0);
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("voltage,current,power");
 }
 
 void loop() {
   float measurement = 0;  // variable to store the value coming from the sensor
 
-  // TODO if we keep the output_csv function then we don't need these
-  float voltage = 0.00;
-  float analog;
-  float power;
-  float vout;
-  float current;
-
   /*
    * V = 5D / 1024
   */
 
+ // Get voltage mesurement
   measurement = analogRead(voltagePin);
   if (measurement > MIN_VOLT_VAL) {
-    // Serial.print("received (voltage) = ");
-    // Serial.println(measurement);
     voltageArray.push_back(measurement);
-    Serial.print("time in miliseconds (before): ");
-    Serial.println(millis());
 
-    
+    // Get current measurement
     measurement = analogRead(currentPin);
-    // Serial.print("received (current) = ");
-    // Serial.println(measurement);
     currentArray.push_back(measurement);
 
     // Take samples from ADC
     for (int i = 0; i < SAMPLES; i++) {
-      // Serial.println("hello there :)");
+      // Get voltage measurement
       measurement = analogRead(voltagePin);
       voltageArray.push_back(measurement);
 
+      // Get current measurement
       measurement = analogRead(currentPin);
       currentArray.push_back(measurement);
+
       delayMicroseconds(500);
     }
-    Serial.print("time in miliseconds (middle): ");
-    Serial.println(millis());
-    Serial.println();
 
     output_csv(voltageArray, currentArray);
 
-    // DEBUG
-
-    // Serial.println(voltageArray.size());
-    // Serial.println(currentArray.size());
-
-//    // Print sample information to serial
-//    for (int i = 0; i < voltageArray.size(); i++) {
-//      // Output voltage info
-//      Serial.print("raw measurement: ");
-//      Serial.println(voltageArray.at(i));
-//      // 5V is the reference voltage of the ADC on the Arduino Uno
-//      // 1024 is the maximum resolution of ADC (10 bits)
-//      analog = (voltageArray.at(i) * 5.00)/1024.00;
-//      voltage = analog * 0.5;
-//      Serial.print("voltage: ");
-//      Serial.println(voltage, PRECISION);
-//
-//      // Output current info
-//      vout = (currentArray.at(i) * 5.00)/1024.00;
-//      current = float(vout) / float((GAIN*R));
-//      Serial.print("current = ");
-//      Serial.println(current, PRECISION);
-//
-//      // Output power info
-//      power = voltage * current;
-//      Serial.print("power = ");
-//      Serial.println(power, 9); // nano resolution
-//      Serial.println();
-//    }
-    Serial.println();
-    Serial.print("time in miliseconds (after): ");
-    Serial.println(millis());
-
+    // Clear the arrays to get a new sample
     voltageArray.clear();
     currentArray.clear();
   }
@@ -112,10 +73,10 @@ void loop() {
  * Write the voltage, current, and power values from the two arrays to serial in CSV format
  */
 void output_csv(Vector<int> voltageArray, Vector<int> currentArray) {
-  // TODO see if we want to print this header only once or keep it this way
-  Serial.println("voltage,current,power");
-
   for (int i = 0; i < voltageArray.size(); i++) {
+    // 5V is the reference voltage of the ADC on the Arduino Uno
+    // 1024 is the maximum resolution of ADC (10 bits)
+    
     // Calculate voltage
     float analog = (voltageArray.at(i) * 5.00)/1024.00;
     float voltage = analog * 0.5;     // Only if voltage comes from the VOLT 2X SENSE pin
